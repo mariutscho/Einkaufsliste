1662fd3b163b72849703a954192d3444d84fe26a
Mario Zimmermann <mario.zimmermann@outlook.de>
2019-03-30 05:56:19 +0100

    Switch auf Mobile Masterpage abgeschaltet

--- EinkaufslisteV2/Site.Mobile.Master.cs
+++ EinkaufslisteV2/Site.Mobile.Master.cs
@@ -11,6 +11,12 @@ namespace EinkaufslisteV2
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+        //http://adicodes.com/tip-disable-mobile-master-page-in-asp-net/
+            var AlternateView = "Desktop";
+            var switchViewRouteName = "AspNet.FriendlyUrls.SwitchView";
+            var url = GetRouteUrl(switchViewRouteName, new { view = AlternateView, __FriendlyUrls_SwitchViews = true });
+            url += "?ReturnUrl=" + HttpUtility.UrlEncode(Request.RawUrl);
+            Response.Redirect(url);
 
         }
     }
